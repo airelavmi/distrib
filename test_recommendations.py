19898f3b6f9fa580e9fd68086c82d03ea966d1b2
airelavmi <146780260+airelavmi@users.noreply.github.com>
2024-05-09 19:22:54 +0300

    Update test_recommendations.py

--- test_recommendations.py
+++ test_recommendations.py
@@ -4,7 +4,7 @@ import requests
 class TestRecommendationsAPI:
     @pytest.fixture(scope='module')
     def api_url(self):
-        return "http://my_microservice:5000/recommend"
+        return "http://microservice_lr3:5000/recommend"
 
     def test_recommendations_for_genre(self, api_url):
         response = requests.get(api_url, params={"genre": "Detective"})
