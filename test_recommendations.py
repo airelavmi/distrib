479508aa0f7627f9d457e4f42323105dad2b6d71
airelavmi <146780260+airelavmi@users.noreply.github.com>
2024-05-09 23:21:01 +0300

    Update test_recommendations.py

--- test_recommendations.py
+++ test_recommendations.py
@@ -7,11 +7,25 @@ class TestRecommendationsAPI:
         return "http://127.0.0.1:5000/recommend"
 
     def test_recommendations_for_genre(self, api_url):
-        response = requests.get(api_url, params={"genre": "Detective"})
+        response = requests.get(api_url, params={"genre": "Fantastic"})
         assert response.status_code == 200
+        expected_response = [
+            ["1984", 1.0],
+            ["The Martian", 1.0],
+            ["Neuromancer", 1.0],
+            ["A Time to Live and a Time to Die", 1.0],
+            ["Childhood's End", 1.0]
+        ]
+        assert response.json() == expected_response
 
     def test_recommendations_for_author(self, api_url):
         response = requests.get(api_url, params={"author": "George Orwell"})
         assert response.status_code == 200
-
-
+        expected_response = [
+            ["1984", 1.0000000000000002],
+            ["The Martian", 0.0],
+            ["Neuromancer", 0.0],
+            ["A Time to Live and a Time to Die", 0.0],
+            ["Childhood's End", 0.0]
+        ]
+        assert response.json() == expected_response
